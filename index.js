f5a4f76e09cccab3414b2c66a016707829bfa7fc
Jake Hiller <jakemhiller@gmail.com>
2018-06-21 10:57:09 -0400

    initial attempt

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 module.exports = {
   presets: [
-    require('babel-preset-es2015'),
-    require('babel-preset-react'),
-    require('babel-preset-stage-0')
+    require('babel-preset-env').default(null, {
+      debug: false,
+      modules: false,
+      targets: {
+        android: 30,
+        chrome: 35,
+        edge: 14,
+        explorer: 11,
+        firefox: 52,
+        safari: 9
+      }
+    }),
+    require('babel-preset-stage-0'),
+    require('babel-preset-react')
   ],
   plugins: [
-    require('babel-plugin-add-module-exports'),
-    require('babel-plugin-transform-decorators-legacy').default,
-    require('babel-plugin-transform-es2015-template-literals'),
+    [require('babel-plugin-transform-es2015-template-literals'), {
+      spec: true
+    }],
+    require('babel-plugin-transform-es5-property-mutators'),
     require('babel-plugin-transform-es3-member-expression-literals'),
+    require('babel-plugin-transform-decorators-legacy').default,
     require('babel-plugin-transform-es3-property-literals'),
     require('babel-plugin-transform-jscript'),
-    require('babel-plugin-transform-exponentiation-operator')
   ]
-};
+}
