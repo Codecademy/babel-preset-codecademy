2f1aff69ea63b17959f1324305a5532ea543f488
Jake Hiller <jakemhiller@gmail.com>
2018-06-26 12:38:17 -0400

    cleanup

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ module.exports = {
     require('babel-preset-react')
   ],
   plugins: [
-    [require('babel-plugin-transform-es2015-template-literals'), {
-      spec: true
-    }],
-    require('babel-plugin-transform-es5-property-mutators'),
-    require('babel-plugin-transform-es3-member-expression-literals'),
     require('babel-plugin-transform-decorators-legacy').default,
-    require('babel-plugin-transform-es3-property-literals'),
     require('babel-plugin-transform-jscript'),
   ]
 }
