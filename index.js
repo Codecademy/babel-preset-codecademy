af311391ad5d285ca374a8b32dda14aacd95ca97
Jake Hiller <jakemhiller@gmail.com>
2018-06-26 10:42:18 -0400

    utilize react-app preset because it so closely matches our config

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
+const env = process.env.BABEL_ENV || process.env.NODE_ENV;
+const isEnvTest = env === 'test';
+const isEnvProduction = env === 'production';
+const isEnvDevelopment = !isEnvTest && !isEnvProduction;
+
 module.exports = {
   presets: [
-    [require('babel-preset-env').default, {
-      debug: false,
-      modules: false,
+    'react-app',
+    [require('babel-preset-stage-1'), {
+      decoratorsLegacy: true
     }],
-    require('babel-preset-stage-0'),
-    require('babel-preset-react')
-  ],
-  plugins: [
-    [require('babel-plugin-transform-es2015-template-literals'), {
-      spec: true
-    }],
-    require('babel-plugin-transform-es5-property-mutators'),
-    require('babel-plugin-transform-es3-member-expression-literals'),
-    require('babel-plugin-transform-decorators-legacy').default,
-    require('babel-plugin-transform-es3-property-literals'),
-    require('babel-plugin-transform-jscript'),
   ]
 }
