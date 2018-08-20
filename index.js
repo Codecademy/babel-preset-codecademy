67351cd0afe87aaf59078ef90a319f38bf9a45ee
Jake Hiller <jakemhiller@gmail.com>
2018-08-20 14:11:02 -0400

    remove stage-1

--- index.js
+++ index.js
@@ -1,62 +1,63 @@
 const env = process.env.BABEL_ENV || process.env.NODE_ENV;
-const isEnvTest = env === 'test';
-const isEnvProduction = env === 'production';
+const isEnvTest = env === "test";
+const isEnvProduction = env === "production";
 const isEnvDevelopment = !isEnvTest && !isEnvProduction;
 
 module.exports = {
   presets: [
     isEnvTest && [
-      require('@babel/preset-env').default,
+      require("@babel/preset-env").default,
       {
         targets: {
-          node: 'current',
-        },
-      },
+          node: "current"
+        }
+      }
     ],
     (isEnvProduction || isEnvDevelopment) && [
-      require('@babel/preset-env').default,
+      require("@babel/preset-env").default,
       {
-        useBuiltIns: 'entry',
-        modules: false,
-      },
+        useBuiltIns: "entry",
+        modules: false
+      }
     ],
-    [require('@babel/preset-stage-1').default, {
-      decoratorsLegacy: true
-    }],
-    require('@babel/preset-react').default
+    require("@babel/preset-react").default
   ].filter(Boolean),
   plugins: [
-    require('@babel/plugin-transform-destructuring').default,
+    require("@babel/plugin-transform-destructuring").default,
     [
-      require('@babel/plugin-proposal-class-properties').default,
+      require("@babel/plugin-proposal-class-properties").default,
       {
-        loose: true,
-      },
+        loose: true
+      }
     ],
     [
-      require('@babel/plugin-proposal-object-rest-spread').default,
+      require("@babel/plugin-proposal-object-rest-spread").default,
       {
-        useBuiltIns: true,
-      },
+        useBuiltIns: true
+      }
     ],
+    require("@babel/plugin-proposal-decorators"),
+    require("@babel/plugin-proposal-export-default-from"),
+    require("@babel/plugin-proposal-export-namespace-from"),
+    require("@babel/plugin-syntax-import-meta"),
     [
-      require('@babel/plugin-transform-runtime').default,
+      require("@babel/plugin-transform-runtime").default,
       {
         helpers: false,
         polyfill: false,
-        regenerator: true,
-      },
+        regenerator: true
+      }
     ],
     !isEnvTest && [
-      require('@babel/plugin-transform-regenerator').default,
+      require("@babel/plugin-transform-regenerator").default,
       {
         // Async functions are converted to generators by @babel/preset-env
-        async: false,
-      },
+        async: false
+      }
     ],
-    require('@babel/plugin-syntax-dynamic-import').default,
+    require("@babel/plugin-syntax-dynamic-import").default,
     isEnvTest &&
       // Transform dynamic import to require
-      require('babel-plugin-transform-dynamic-import').default,
+      require("babel-plugin-transform-dynamic-import").default
   ].filter(Boolean)
-}
+};
